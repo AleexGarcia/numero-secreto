25197cc51a1ad7702efd18bedb7dc1121e43e265
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-15 15:47:50 -0300

    criando logica para sorteio e manipulando o DOM para exibir o menor e maior valor usados no JS

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,1 +1,13 @@
-console.log('Hello')
+const menorValor = 1 ;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMenorValor.innerHTML = maiorValor;
