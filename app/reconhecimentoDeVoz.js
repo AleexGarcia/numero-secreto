6e4ebd0189ba8e99e0b16947f08755524cf02b66
Alexandre Garcia <97323322+AleexGarcia@users.noreply.github.com>
2022-08-24 20:35:30 -0300

    tentativa 2 playagain

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -2,32 +2,31 @@ const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
- const recognition = new SpeechRecognition();
+const recognition = new SpeechRecognition();
 
- recognition.lang = 'pt-Br';
- recognition.start();
+recognition.lang = 'pt-Br';
+recognition.start();
 
- recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('result', onSpeak)
 
- function onSpeak(e){
-    var chute = e.results[0][0].transcript;
-    exibeChuteNaTela(chute);
-    validaChute(chute);
- }
- 
- function exibeChuteNaTela(chute){
-    elementoChute.innerHTML = `
+function onSpeak(e) {
+   var chute = e.results[0][0].transcript;
+   exibeChuteNaTela(chute);
+   validaChute(chute);
+}
+
+function exibeChuteNaTela(chute) {
+   elementoChute.innerHTML = `
         <div>Você disse:</div>
         <span class="box">${chute}</span>
     `
- }
+}
 
- recognition.addEventListener('end', () => recognition.start())
+recognition.addEventListener('end', () => recognition.start())
 
- const playAgain = document.querySelector(#playAgain);
-   playAgain.addEventListener('click',()=>{
-      gerarNumeroAleatorio();
-      onSpeak(e);
- })
- 
+const playAgain = document.querySelector(#playAgain);
+playAgain.addEventListener('click', (e) => {
+   gerarNumeroAleatorio();
+})
+
 
